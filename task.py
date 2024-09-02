7fb8a73ae0540e4f1f6e23968cb6554e5d0273ec
nicholas1226 <dvc981226@gmail.com>
2024-09-03 02:23:01 +0900

    day 1 practice 3

--- task.py
+++ task.py
@@ -1,3 +1,24 @@
+# input function practice
 # This line of code will take an input using the input() function
-name = input("What is your name?: ")
-print("Hello " + name + "!")
+username = input("What is your name?: ")
+print("Hello " + username + "!")
+
+# len function practice
+length = len(username)
+print(length)
+
+#variable practice
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass2
+glass2 = glass1
+glass1 = temp
+
+print(glass1, glass2)
+
+#day 1 project
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")
