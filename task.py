1fe33c855269c825428bef763bce93e0e393591e
nicholas1226 <dvc981226@gmail.com>
2024-09-03 02:04:22 +0900

    day 1 practice 2

--- task.py
+++ task.py
@@ -1,5 +1,3 @@
-print("Notes from Day 1")
-print("The print statement is used to output strings")
-print("Strings are strings of characters")
-print("String Concatenation is done with the + sign")
-print("New lines can be created with a \ and the letter n")
+# This line of code will take an input using the input() function
+name = input("What is your name?: ")
+print("Hello " + name + "!")
