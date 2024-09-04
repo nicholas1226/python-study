3619d3fdc4dc4c23304665bc4ec5fa4ef794ee54
nicholas1226 <dvc981226@gmail.com>
2024-09-04 14:51:12 +0900

    day3 practice2

---
+++ python_bootcamp_3/python_bootcamp_3.py
@@ -0,0 +1,6 @@
+# Pizza Deliveries Practice
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L\n")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N\n")
+extra_cheeze = input("Do you want extra cheeze? Y or N\n")
+
