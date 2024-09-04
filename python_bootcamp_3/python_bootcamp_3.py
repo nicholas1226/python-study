06cc29ff57bee1ad06c1016652849955bf2d28b3
nicholas1226 <dvc981226@gmail.com>
2024-09-04 16:54:59 +0900

    day3 practice3

--- python_bootcamp_3/python_bootcamp_3.py
+++ python_bootcamp_3/python_bootcamp_3.py
@@ -5,26 +5,24 @@ size = input("What size pizza do you want? S, M, or L\n")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N\n")
 extra_cheese = input("Do you want extra cheese? Y or N\n")
 
+bill = 0
 # todo: work out how much they need to pay based on their size choice.
-# todo: work out how much to add to their bill based on their pepperoni choice.
-# todo: work out their final amount based on whether if they want extra cheese.
-
-final_amount = 0
-
 if size == "S":
-    final_amount += 15
-    if pepperoni == "Y":
-        final_amount += 2
-    if extra_cheese == "Y":
-        final_amount += 1
-if size == "M" or "L":
-    if size == "M":
-        final_amount += 20
-    if size == "L":
-        final_amount += 25
-    if pepperoni == "Y":
-        final_amount +=3
-    if extra_cheese == "Y":
-        final_amount += 1
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
 
-print(f"${final_amount}")
+# todo: work out how much to add to their bill based on their pepperoni choice.
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+# todo: work out their final amount based on whether if they want extra cheese.
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"${bill}")
